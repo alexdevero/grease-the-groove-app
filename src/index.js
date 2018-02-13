a1bccacd028235ea188fb596b055e2e6afe4ea4d
Alex Devero <deveroalex@gmail.com>
2018-02-13 08:27:07 +0100

    Implement font Hind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
+import styled, { injectGlobal } from 'styled-components'
+
 import App from './app/App'
+
+const hindBold = require('./assets/fonts/Hind-Bold.ttf')
+const hindLight = require('./assets/fonts/Hind-Light.ttf')
+const hindRegular = require('./assets/fonts/Hind-Regular.ttf')
+
+injectGlobal`
+	@font-face {
+    font-family: 'Hind';
+		font-weight: 300;
+    src: url(${hindLight});
+  }
+
+	@font-face {
+    font-family: 'Hind';
+		font-weight: 400;
+    src: url(${hindRegular});
+  }
+
+	@font-face {
+    font-family: 'Hind';
+		font-weight: 700;
+    src: url(${hindBold});
+  }
+
+	body {
+		font-family: 'Hind', serif;
+	}
+`
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
