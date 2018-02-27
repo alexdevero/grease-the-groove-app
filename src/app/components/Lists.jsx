1aaf19950852e6fe2b9bab758ee9feebe37141a8
Alex Devero <deveroalex@gmail.com>
2018-02-27 10:00:48 +0100

    Improve spacing of list items

--- src/app/components/Lists.jsx
+++ src/app/components/Lists.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 
 export const ListUnstyled = styled.ul`
   padding: 0;
-  margin: 8px 0 0;
+  margin: 10px 0 0;
   list-style-type: none;
+  
+  li + li {
+    margin-top: 6px;
+  }
 `
