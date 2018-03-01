c00bb1b47f9515aa69f0222235e771e2f7eb57e7
Alex Devero <deveroalex@gmail.com>
2018-03-01 09:51:20 +0100

    Change alignment of nav links

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Icon, IconWraper } from './Icon'
 
 const NavWrapper = styled.nav`
+  padding-top: 12px;
+  padding-bottom: 12px;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   width: 100%;
+  
+  ${props => props.hasCrossSettings && css`
+    justify-content: flex-end;
+  `}
 `
 
 const Nav = (props) => {
   return(
-    <NavWrapper>
+    <NavWrapper hasCrossSettings={props.hasCrossSettings}>
       {props.hasTimer && <IconWraper href="#" onClick={props.toggleTimer}>
         <Icon clock />
       </IconWraper>}
