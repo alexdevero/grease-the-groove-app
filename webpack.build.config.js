c00c3798b28c7d094ff64564a5ddee7c46864d5a
Alex Devero <deveroalex@gmail.com>
2018-03-06 08:51:35 +0100

    Fix missing title

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -46,7 +46,9 @@ module.exports = {
   },
   target: 'electron-renderer',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Grease the Groove"
+    }),
     new ExtractTextPlugin('bundle.css'),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -45,7 +45,9 @@ module.exports = {
   },
   target: 'electron-renderer',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Grease the Groove"
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
