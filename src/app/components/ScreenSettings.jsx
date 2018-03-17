fc1f2ba7a15395ae8c0ffbf9efff0006820987bc
Alex Devero <deveroalex@gmail.com>
2018-03-17 15:22:44 +0100

    Change label of settings button

--- src/app/components/ScreenSettings.jsx
+++ src/app/components/ScreenSettings.jsx
@@ -35,7 +35,7 @@ const ScreenSettings = (props) => {
       </InputWrapper>
 
       {props.isOnboarding && <ButtonWrapper center>
-        <Button onClick={props.saveSettings}>Get started!</Button>
+        <Button onClick={props.saveSettings}>Save & start!</Button>
       </ButtonWrapper>}
     </ScreenWrapper>
   )
