a01cc87672b28e056233c7b98de98b87c0568c4b
Alex Devero <deveroalex@gmail.com>
2018-03-17 11:51:17 +0100

    Remove option for timer

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -4,25 +4,16 @@ import styled, { css } from 'styled-components'
 import { Icon, IconWraper } from './Icon'
 
 const NavWrapper = styled.nav`
-  padding-top: 12px;
   padding-bottom: 12px;
   display: flex;
   align-items: center;
   justify-content: flex-end;
   width: 100%;
-
-  ${'' /* ${props => props.hasCrossSettings && css`
-    justify-content: flex-end;
-  `} */}
 `
 
 const Nav = (props) => {
   return(
     <NavWrapper hasCrossSettings={props.hasCrossSettings}>
-      {/* {props.hasTimer && <IconWraper href="#" onClick={props.toggleTimer}>
-        <Icon clock />
-      </IconWraper>} */}
-
       {props.hasMenu && <IconWraper href="#" onClick={props.toggleSettings}>
         <Icon menu />
       </IconWraper>}
