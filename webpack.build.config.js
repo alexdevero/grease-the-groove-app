f91bcbcf8bc5775244c06095fd6d1bb6d3ac3683
Alex Devero <deveroalex@gmail.com>
2019-08-25 10:37:39 +0200

    Replace extract-text with mini-css-extract plugin

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BabiliPlugin = require('babili-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
 const defaultInclude = path.resolve(__dirname, 'src')
@@ -39,7 +39,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Grease the Groove"
     }),
-    new ExtractTextPlugin('bundle.css'),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
