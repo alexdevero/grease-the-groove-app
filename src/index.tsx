bbc1e5197b0ac7055b01eb994d82381b0bdfee19
Alex Devero <deveroalex@gmail.com>
2019-09-06 08:40:49 +0200

    Rename all js components to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -9,27 +9,27 @@ const hindLight = require('./assets/fonts/Hind-Light.ttf')
 const hindRegular = require('./assets/fonts/Hind-Regular.ttf')
 
 const GlobalStyle = createGlobalStyle`
-	@font-face {
+  @font-face {
     font-family: 'Hind';
-		font-weight: 300;
+    font-weight: 300;
     src: url(${hindLight});
   }
 
-	@font-face {
+  @font-face {
     font-family: 'Hind';
-		font-weight: 400;
+    font-weight: 400;
     src: url(${hindRegular});
   }
 
-	@font-face {
+  @font-face {
     font-family: 'Hind';
-		font-weight: 700;
+    font-weight: 700;
     src: url(${hindBold});
   }
 
-	body {
-		font: 100% / 1.618 'Hind', serif;
-	}
+  body {
+    font: 100% / 1.618 'Hind', serif;
+  }
 
   * {
     -webkit-user-select: none;
