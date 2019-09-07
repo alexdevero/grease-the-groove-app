2148518b00d30b01729ee66b1076ec77c6f0085c
Alex Devero <deveroalex@gmail.com>
2019-09-07 10:53:11 +0200

    Add interface for checkbox component

--- src/app/components/Checkbox.tsx
+++ src/app/components/Checkbox.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
+interface CheckboxInterface {
+  id?: string;
+  label?: string;
+}
+
 const LabelEl = styled.label`
   margin-bottom: 0;
   user-select: none;
@@ -86,16 +91,16 @@ const LabelEl = styled.label`
   }
 `
 
-const Checkbox = ({id, label}) => {
-  return(
+const Checkbox = ({ id, label }: CheckboxInterface) => {
+  return (
     <LabelEl htmlFor={id} className="label-cbx">
       <input id={id} type="checkbox" className="invisible" />
 
       <div className="checkbox">
         <svg width="20px" height="20px" viewBox="0 0 20 20">
-          <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"></path>
+          <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z" />
 
-          <polyline points="4 11 8 15 16 6"></polyline>
+          <polyline points="4 11 8 15 16 6" />
         </svg>
       </div>
 
