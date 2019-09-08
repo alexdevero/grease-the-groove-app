5a5021448e629663808b9e592905b6bcd105550e
Alex Devero <deveroalex@gmail.com>
2019-09-08 05:42:45 +0200

    Fix input values in settings

--- src/app/components/ScreenSettings.tsx
+++ src/app/components/ScreenSettings.tsx
@@ -28,7 +28,7 @@ const ScreenSettings = (props: ScreenSettingsInterface) => {
       <Text>How many sets do you want to do?</Text>
 
       <InputWrapper first>
-        <Input className="settings-sets" type="number" defaultValue={props.numOfSets} />
+        <Input className="settings-sets" type="number" defaultValue={props.numOfSets.toString()} />
 
         {!props.isOnboarding && <Button last onClick={props.handleSetsChange}>Save</Button>}
       </InputWrapper>
@@ -38,7 +38,7 @@ const ScreenSettings = (props: ScreenSettingsInterface) => {
       <Text>How long should the rest pause be (in minutes)? You can use decimal numbers for seconds, i.e.: 0.2 for 12s.</Text>
 
       <InputWrapper>
-        <Input className="settings-pause" type="number" defaultValue={props.restPauseLength} />
+        <Input className="settings-pause" type="number" defaultValue={props.restPauseLength.toString()} />
 
         {!props.isOnboarding && <Button last onClick={props.handlePauseLengthChange}>Save</Button>}
       </InputWrapper>
